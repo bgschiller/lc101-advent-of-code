9024d7a85503cce57b27d57b03e4c67cf8f2c4c8
Brian Schiller <bgschiller@gmail.com>
2016-11-29 09:35:17 -0600

    Add some error handlers

--- advent_of_code/views.py
+++ advent_of_code/views.py
@@ -1,14 +1,22 @@
 from . import app
-from flask import url_for, render_template, session
-from .permissions import require_user_has_solved, require_advent_day_reached
+from flask import url_for, render_template, session, redirect
+from .permissions import require_user_has_solved, require_advent_day_reached, NotYetPublishedError, NotYetSolvedError
 from .forms import BoxableChallenge
 @app.route('/')
 def index():
     ''' list of challenges '''
     return render_template('index.html')
 
+@app.errorhandler(NotYetPublishedError)
+def not_yet_published_redir(error):
+    return redirect(url_for('not_yet_published'))
+
+@app.errorhandler(NotYetSolvedError)
+def not_yet_solved_redir(error):
+    return redirect(url_for('challenges_view', day=error.args[0]))
+
 @app.route('/challenges/<int:day>', methods=['GET', 'OPTIONS', 'POST'])
-#@require_advent_day_reached
+@require_advent_day_reached
 def challenges_view(day):
     form = BoxableChallenge()
     if form.validate_on_submit():
